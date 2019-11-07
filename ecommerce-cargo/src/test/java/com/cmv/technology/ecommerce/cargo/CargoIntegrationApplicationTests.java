0a8e0fdcb6ec3b7dccebc6bb6eb15e72b7ed1d49
kutsal <mkutsalyilmaz@hotmail.com>
2019-11-08 01:29:10 +0300

    template not finished

---
+++ ecommerce-cargo/src/test/java/com/cmv/technology/ecommerce/cargo/CargoIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cmv.technology.ecommerce.cargo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CargoIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ ecommerce-config-server/src/test/java/com/cmv/technology/ecommerce/config/server/EcommerceConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cmv.technology.ecommerce.config.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
