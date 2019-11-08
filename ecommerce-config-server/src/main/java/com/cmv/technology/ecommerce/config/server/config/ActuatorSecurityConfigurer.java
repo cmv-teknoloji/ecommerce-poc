6cc02f96b7c01e22c270e841bf4368aba9defb77
kutsal <mkutsalyilmaz@hotmail.com>
2019-11-09 02:07:53 +0300

    properties to yaml

---
+++ ecommerce-config-server/src/main/java/com/cmv/technology/ecommerce/config/server/config/ActuatorSecurityConfigurer.java
@@ -0,0 +1,14 @@
+package com.cmv.technology.ecommerce.config.server.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class ActuatorSecurityConfigurer extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable().httpBasic();
+    }
+}
